3fd56126eb5067ec290a54e63a148263677efb2a
PETER <petrkateeb4@gmail.com>
2023-09-10 03:57:35 -0700

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/pyrhon3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        if idx < 0 or idx > len(my_list) - 1:
-            return my_list
-        else:
-            return my_list.insert(idx, element)
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
